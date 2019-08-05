adb583c6ace82767f84ff99622cecd670a213684
vdphan <duyphan0@gmail.com>
2019-08-05 22:57:47 +0000

    fix puppet-lint error

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # Using Puppet, create a manifest that kills a process named killmenow
 exec { 'killmenow':
-    cwd => '/home/vagrant/holberton-system_engineering-devops/0x0A-configuration_management',
     command => 'pkill -f ./killmenow',
-    path => '/usr/bin',
+    path    => '/usr/bin',
 }
