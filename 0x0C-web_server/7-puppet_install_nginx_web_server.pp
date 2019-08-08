2fdcf58c16dc1159d2a30ef881b39200c93f5365
vdphan <duyphan0@gmail.com>
2019-08-08 17:28:35 +0000

    fix quotes

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,5 +3,5 @@ exec {'/usr/bin/env apt-get -y update':}
 exec {'/usr/bin/env apt-get -y install nginx':}
 exec {"/usr/bin/env ufw allow 'Nginx HTTP'":}
 exec {'/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html':}
-exec {"/usr/bin/env sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default":}
+exec {'/usr/bin/env sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default':}
 exec {'/usr/bin/env execservice nginx start':}
