f4cebcd3d9ed70da9b0ad968c174e5651d868ac9
vdphan <duyphan0@gmail.com>
2019-09-19 19:28:45 +0000

    fix comment

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,9 +9,9 @@ import requests
 def top_ten(subreddit):
     """prints the titles of the first 10 hot posts
     for a given subreddit"""
-    """headers = {"User-Agent": "Mozilla/5.0
-    (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36
-    (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"}"""
+    #headers = {"User-Agent": "Mozilla/5.0
+    #(Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36
+    #(KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"}"""
     r = requests.get('https://www.reddit.com/r/{}/hot.json'.
                      format(subreddit))
     if r.status_code == 200 and r.json().get('data').get('children'):
