7446b70439ed9e68910a68d0f0d0deefb251157c
vdphan <duyphan0@gmail.com>
2019-09-19 19:32:34 +0000

    add headers

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,11 +9,12 @@ import requests
 def top_ten(subreddit):
     """prints the titles of the first 10 hot posts
     for a given subreddit"""
-    #headers = {"User-Agent": "Mozilla/5.0
-    #(Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36
-    #(KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"}"""
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh;" +
+               "Intel Mac OS X 10_14_5)" +
+               "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132" +
+               "Safari/537.36"}
     r = requests.get('https://www.reddit.com/r/{}/hot.json'.
-                     format(subreddit))
+                     format(subreddit), headers=headers)
     if r.status_code == 200 and r.json().get('data').get('children'):
         c = r.json().get('data').get('children')
         l = len(c)
