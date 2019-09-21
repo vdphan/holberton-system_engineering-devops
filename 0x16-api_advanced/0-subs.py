c22fa2de3ed2211bdbdef39ab2cfa19a63564097
vdphan <duyphan0@gmail.com>
2019-09-21 04:59:17 +0000

    add header

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh;" +
+               "Intel Mac OS X 10_14_5)" +
+               "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132" +
+               "Safari/537.36"}
     r = requests.get('https://www.reddit.com/r/{}/about.json'.
-                     format(subreddit))
-    if r.status_code == 200 and r.json().get('data').get('subscribers'):
+                     format(subreddit), headers=headers)
+    if r.status_code == 200:
         return r.json().get('data').get('subscribers')
     return 0
