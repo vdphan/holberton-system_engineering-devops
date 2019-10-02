f40764c1574c5cc97ffb5fc9c67bde452e519fca
vdphan <duyphan0@gmail.com>
2019-10-02 17:03:28 +0000

    add comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# replace the typo extension in wp-setting file
 exec { 'killmenow':
     command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
     path    => '/bin',
