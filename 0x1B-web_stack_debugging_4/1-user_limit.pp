0f0ee54d2dbf3b9be1482e71370438396aafea07
vdphan <duyphan0@gmail.com>
2019-10-11 20:31:19 +0000

    add comment

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,4 @@
+# login with the holberton user and open a file without any error message
 exec { 'delete holberton user':
     command => "sed -i '/holberton hard nofile 5/d' /etc/security/limits.conf",
     path    => '/bin/',
